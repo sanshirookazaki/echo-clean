2bbed658b647d88ebe72facecb5d91f56f58c82e
sanshiro_okazaki <sanshirookazaki@gmail.com>
2019-02-10 23:33:28 +0900

    add sqlhandler

--- infrastructure/sqlhandler.go
+++ infrastructure/sqlhandler.go
@@ -2,11 +2,9 @@ package infrastructure
 
 import (
 	"database/sql"
-	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
-
-	"github.com/sanshirookazaki/echo-clean/domain"
+	"github.com/sanshirookazaki/echo-clean/interfaces/database"
 )
 
 // SQLHandler .
@@ -25,18 +23,50 @@ func NewSQLHandler() *SQLHandler {
 	return sqlHandler
 }
 
-func (h *SQLHandler) GetTaskAll(userid int) domain.Tasks {
-	rows, err := h.Conn.Query("SELECT * FROM tasks WHERE Status = 0 and userid = " + strconv.Itoa(userid))
-	var tasks domain.Tasks
+func (handler *SQLHandler) Execute(statement string, args ...interface{}) (database.Result, error) {
+	res := SQLResult{}
+	result, err := handler.Conn.Exec(statement, args...)
 	if err != nil {
-		panic(err.Error())
+		return res, err
 	}
-	defer rows.Close()
-	for rows.Next() {
-		if err := rows.Scan(&t.ID, &t.Userid, &t.Task, &t.Status); err != nil {
-			panic(err.Error())
-		}
-		tasks = append(tasks, *t)
+	res.Result = result
+	return res, nil
+}
+
+func (handler *SQLHandler) Query(statement string, args ...interface{}) (database.Row, error) {
+	rows, err := handler.Conn.Query(statement, args...)
+	if err != nil {
+		return new(SqlRow), err
 	}
-	return tasks
+	row := new(SqlRow)
+	row.Rows = rows
+	return row, nil
+}
+
+type SQLResult struct {
+	Result sql.Result
+}
+
+func (r SQLResult) LastInsertId() (int64, error) {
+	return r.Result.LastInsertId()
+}
+
+func (r SQLResult) RowsAffected() (int64, error) {
+	return r.Result.RowsAffected()
+}
+
+type SQLRow struct {
+	Rows *sql.Rows
+}
+
+func (r SQLRow) Scan(dest ...interface{}) error {
+	return r.Rows.Scan(dest...)
+}
+
+func (r SQLRow) Next() bool {
+	return r.Rows.Next()
+}
+
+func (r SQLRow) Close() error {
+	return r.Rows.Close()
 }
