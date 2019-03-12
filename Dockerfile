7e0bccebadbb72b86c78ff716ce5e125f146f0e7
sanshiro_okazaki <sanshirookazaki@gmail.com>
2019-03-13 00:45:10 +0900

    update infra

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM mysql:5.7
-LABEL maintainer "s.okazaki"
-
-COPY ./sample.sql /docker-entrypoint-initdb.d/echo-sample.sql
-
--- infrastructure/mysql/init.sql
+++ infrastructure/mysql/init.sql
@@ -3,11 +3,11 @@ use task;
 
 CREATE TABLE tasks ( id int NOT NULL AUTO_INCREMENT,
                      userid int NOT NULL,
-                     task varchar(25) NOT NULL, 
+                     task varchar(100) NOT NULL, 
                      status int NOT NULL, 
                      primary key(id));
 
 CREATE TABLE users ( userid int NOT NULL AUTO_INCREMENT,
                      username varchar(20) NOT NULL,
-                     password varchar(255) NOT NULL,
+                     password varchar(100) NOT NULL,
                      primary key(userid, username));
