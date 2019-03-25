425bc283430be5c1f2da2ed9230b1631b7c482d1
sanshiro_okazaki <sanshirookazaki@gmail.com>
2019-03-25 23:09:36 +0900

    fix gorm

--- infrastructure/sqlhandler.go
+++ infrastructure/sqlhandler.go
@@ -26,28 +26,3 @@ func NewSQLHandler() *SQLHandler {
 	return sqlHandler
 }
 
-func (handler *SQLHandler) Query(statement string, args ...interface{}) (database.Row, error) {
-	rows, err := handler.Conn.Query(statement, args...)
-	if err != nil {
-		return new(SQLRow), err
-	}
-	row := new(SQLRow)
-	row.Rows = rows
-	return row, nil
-}
-
-type SQLRow struct {
-	Rows *sql.Rows
-}
-
-func (r SQLRow) Scan(dest ...interface{}) error {
-	return r.Rows.Scan(dest...)
-}
-
-func (r SQLRow) Next() bool {
-	return r.Rows.Next()
-}
-
-func (r SQLRow) Close() error {
-	return r.Rows.Close()
-}
