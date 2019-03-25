d76807206383df889f154f8f5805657ebcd10c09
sanshiro_okazaki <sanshirookazaki@gmail.com>
2019-03-26 00:59:31 +0900

    fix sql handler

--- interfaces/database/sql_handler.go
+++ interfaces/database/sql_handler.go
@@ -1,11 +1,4 @@
 package database
 
 type SQLHandler interface {
-	Query(string, ...interface{}) (Row, error)
-}
-
-type Row interface {
-	Scan(...interface{}) error
-	Next() bool
-	Close() error
 }
