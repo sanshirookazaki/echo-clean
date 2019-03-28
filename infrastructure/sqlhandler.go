74cc08b16eb417ded8cb58cf46fe8a3a07fde482
sanshiro_okazaki <sanshirookazaki@gmail.com>
2019-03-28 23:27:40 +0900

    update sqlhandler

--- infrastructure/sqlhandler.go
+++ infrastructure/sqlhandler.go
@@ -6,7 +6,6 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	//_ "github.com/go-sql-driver/mysql"
-	"github.com/sanshirookazaki/echo-clean/interfaces/database"
 )
 
 // SQLHandler .
@@ -26,3 +25,22 @@ func NewSQLHandler() *SQLHandler {
 	return sqlHandler
 }
 
+func (handler *SQLHandler) Table(statement string) *SQLHandler {
+	return handler.Conn.Table(statement)
+}
+
+func (handler *SQLHandler) Select(query interface{}, args ...interface{}) *SQLHandler {
+	return handler.Conn.Select(query, args...)
+}
+
+func (handler *SQLHandler) Where(query interface{}, args ...interface{}) *SQLHandler {
+	return handler.Conn.Where(query, args...)
+}
+
+func (handler *SQLHandler) Scan(dest interface{}) *SQLHandler {
+	return handler.Conn.Scan(dest)
+}
+
+func (handler *SQLHandler) Create(value interface{}) *SQLHandler {
+	return handler.Conn.Create(value)
+}
