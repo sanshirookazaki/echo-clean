0ec6f0e769f8953aae9bd999fb93ed1d1a605781
sanshiro_okazaki <sanshirookazaki@gmail.com>
2019-03-29 23:17:06 +0900

    update sql handler

--- interfaces/database/sql_handler.go
+++ interfaces/database/sql_handler.go
@@ -1,4 +1,9 @@
 package database
 
 type SQLHandler interface {
+	Table(string) SQLHandler
+	Select(interface{}, ...interface{}) SQLHandler
+	Where(interface{}, ...interface{}) SQLHandler
+	Scan(interface{}) SQLHandler
+	Create(interface{}) SQLHandler
 }
