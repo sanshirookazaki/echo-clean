8e9b0912d85e1faf02d98adabedbe850a7a623e4
sanshirookazaki <sanshirookazaki@gmail.com>
2019-04-23 01:02:30 +0900

    update vue

---
+++ vue/hello.vue
@@ -0,0 +1,15 @@
+<template>
+ <p class="message">message :{{msg}} </p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {msg: 'hello'}
+    }
+}
+</script>
+
+<style>
+.message {color: '#42b983'}
+</style>
