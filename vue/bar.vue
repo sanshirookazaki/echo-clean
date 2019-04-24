8f3a647b07fc26ae5a42fa4c605644f747f499d5
sanshirookazaki <sanshirookazaki@gmail.com>
2019-04-24 19:27:52 +0900

    add vue files

---
+++ vue/bar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bar">
+    <h1 class="header"> Bar component</h1>
+    <p>This is Bar component</p>
+  </div>
+</template>
+
+<style>
+  p {text-decoration: underline;}
+</style>
+
+<style scoped>
+  .foo {
+      border: solid 1px green;
+      margin: 4px;
+      padding: 4px;
+  }
+  .header {
+      font-size: 150%;
+  }
+</style>
---
+++ vue/foo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="foo">
+    <h1 class="header"> Foo component</h1>
+    <p>This is Foo component</p>
+  </div>
+</template>
+
+<style>
+  p {text-decoration: underline;}
+</style>
+
+<style scoped>
+  .foo {
+      border: solid 1px green;
+      margin: 4px;
+      padding: 4px;
+  }
+  .header {
+      font-size: 150%;
+  }
+</style>
