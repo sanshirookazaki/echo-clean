4373c37fbbcb7082ced46a4dbf93a3d92ce502cb
sanshirookazaki <sanshirookazaki@gmail.com>
2019-04-29 23:45:42 +0900

    update vue

--- vue/tasks/App.vue
+++ vue/tasks/App.vue
@@ -2,20 +2,45 @@
     <div>
         <h2>task lists</h2>
         <ul>
-            <li v-for="task in tasks" v-bind:key="task.id">
+            <li v-for="task in tasks" v-bind:key="task.id" v-on:change="toggleTaskStatus(task)">
                    <input type="checkbox" v-bind:checked="task.done">
                    {{task.name}}
             </li>
         </ul>
+
+        <form v-on:submit.prevent="addTask">
+            <input type="text" v-model="newTaskName" placeholder="new task">
+        </form>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            newTaskName: "",
+        }
+    },
+
     computed: {
         tasks () {
             return this.$store.state.tasks
         },
     },
+
+    methods: {
+        addTask() {
+            this.$store.commit("addTask", {
+                name: this.newTaskName,
+            })
+            this.newTaskName = ""
+        },
+
+        toggleTaskStatus(task) {
+            this.$store.commit("toggleTaskStatus", {
+                id: task.id
+            })
+        },
+    }
 }
 </script>
--- vue/tasks/store.js
+++ vue/tasks/store.js
@@ -9,13 +9,66 @@ const store = new Vuex.Store({
             {
                 id: 1,
                 name: 'buy milk',
+                labelIds: [1, 2],
                 done: false
         },
         {
             id: 2,
             name: 'buy vuejs book',
+            labelIds: [1, 3],
             done: true
         }],
+
+        labels: [
+            {
+                id: 1,
+                text: 'shopping'
+            },
+            {
+                id: 2,
+                text: 'food'
+            },
+            {
+                id: 3,
+                text: 'book'
+            }
+        ],
+
+
+    nextTaskId: 3,
+    nextTaskId: 4,
+    },
+
+    mutations: {
+        addTask (state, {name, labelIds}) {
+            state.tasks.push({
+                id: state.nextTaskId,
+                name,
+                labelIds,
+                done: false
+            })
+
+            state.nextTaskId++
+        },
+
+        toggleTaskStatus (state, {id}) {
+            const filtered = state.tasks.filter(task => {
+                return task.id === id
+            })
+
+        filtered.forEach(task => {
+            task.done = !task.done
+        })
+        },
+
+        addLabel (state, {text}) {
+            state.labels.push({
+                id: state.nextLabelId,
+                text
+            })
+
+            state.nextLabelId++
+        },
     },
 })
 
